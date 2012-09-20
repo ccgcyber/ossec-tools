b9b847dd86959db0ce240c356ca40693cf8603db
Williams <seaw@406c8f1f366e.ant.amazon.com>
2012-09-20 10:42:27 -0700

    Revert "Use env instead of absolute path". Don't need it because even some systems have a different absolute path for `env`
    This reverts commit f1b5dbaada981c60fec8642d1bda3a0f80e2afef.

--- ossec-graph-rules.rb
+++ ossec-graph-rules.rb
@@ -1,57 +1,25 @@
-#!/bin/env ruby
+#!/usr/bin/ruby
 require 'rubygems'
-require 'optparse'
-require 'rgl/dot'
-require 'rgl/adjacency'
 require 'rexml/document'
+require 'rgl/adjacency'
+require 'rgl/dot'
 
-def genGraph(options)
-   rules = []
-   graph = RGL::DirectedAdjacencyGraph.new
-   options[:inputFilenames].each do |f|
-      puts f
-      doc = REXML::Document.new(open(f))
-      rules += doc.elements.to_a('group/rule').collect{ |x| x.attributes['id'] }
-      rules.each do |rule|
-         doc.elements.to_a('group/rule[@id=' + rule + ']').collect{ |x|
-            #If the rule has a parent rule
-            if x.elements['if_sid'] then
-               sid = x.elements['if_sid'].text.to_i
-            #If the rule has a parent rule
-            elsif x.elements['if_matched_sid'] then
-               sid = x.elements['if_matched_sid'].text.to_i
-            # No parent rule
-            else
-               sid = 0
-            end
-            graph.add_edge(sid, rule.to_i)
-         }
+doc = REXML::Document.new(open(ARGV[0]))
+rules = doc.elements.to_a('group/rule').collect{ |x| x.attributes['id'] }
+
+graph = RGL::DirectedAdjacencyGraph.new
+
+rules.each do |rule|
+   sid = doc.elements.to_a('group/rule[@id=' + rule + ']').collect{ |x|
+      if x.elements['if_sid'] then
+         sid = x.elements['if_sid'].text.to_i
+      elsif x.elements['if_matched_sid'] then
+         sid = x.elements['if_matched_sid'].text.to_i
+      else
+         sid = 0
       end
-   end
-   graph.write_to_graphic_file(fmt=options[:graphFormat], dotfile=options[:graphFilename])
-end
-
-if __FILE__ == $0 then
-   options = {
-      :graphFilename => 'graph',
-      :graphFormat => 'png',
+      graph.add_edge sid, rule.to_i
    }
-   OptionParser.new { |opts|
-     opts.banner = "Usage: #{File.basename($0)} -f -t filename <rule_file.xml>"
-
-     opts.on('-f', '--graph-filename FILENAME', 'Output file name for graph') do |arg|
-      options[:graphFilename] = arg
-     end
-
-     opts.on('-t', '--image-format FILENAME', 'File format of graph image. Examples: png, jpg. Default is png.') do |arg|
-       options[:graphFormat] = arg
-     end
-   }.parse!
-
-   if ARGV.empty? then
-      raise OptionParser::MissingArgument, 'Rule file(s)'
-   else
-      options[:inputFilenames] = ARGV
-   end
-   puts "Wrote new graph: %s" % genGraph(options)
 end
+
+puts "Wrote new file: %s" % graph.write_to_graphic_file(fmt='png', dotfile='graphs')
